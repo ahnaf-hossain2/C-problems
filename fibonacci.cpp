51195f346af076a36859dd7fb97d3acb2e375933
ahnaf-hossain2 <ahnaf20042023@gmail.com>
2024-11-17 00:11:00 +0600

    Create fibonacci series with cpp
    2 ways provided to do solve this

---
+++ fibonacci.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+
+    int n, first=0, second=1, fibo;
+
+    cout<<"Enter a Integer : ";
+    cin>>n;
+    cout<<"Fibonacci Series: ";
+
+    for(int count=0; count<n; count++)
+    {
+    if (count<=1)
+	{
+		fibo=count;
+		cout<<fibo<<" ";
+	}
+	else {
+		fibo=first+second;
+		cout<<fibo<<" ";
+		first=second;
+		second=fibo;
+	}
+    }
+
+}
+
+// Easy way and more readable ------------------------------------------------------
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    int n, first = 0, second = 1, count = 0, fibo;
+
+    cout << "Enter an integer: ";
+    cin >> n;
+    cout << "Fibonacci Series: ";
+
+   
+    if (n >= 1) cout << first << " ";
+    if (n >= 2) cout << second << " ";
+
+    for (count = 2; count < n; count++) {
+        fibo = first + second;
+        cout << fibo << " ";
+        first = second;
+        second = fibo;
+    }
+
+    cout << endl;
+    return 0;
+}
